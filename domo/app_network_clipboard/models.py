8cc733958cb45625494548285122d129249d67c3
jo <754514192@qq.com>
2025-03-31 18:29:50 +0800

    更新 models

--- domo/app_network_clipboard/models.py
+++ domo/app_network_clipboard/models.py
@@ -1,8 +1,9 @@
 import uuid
+from datetime import datetime
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import JSONField
 
 from constants.constants import ClipboardPrivacy, ClipboardContentType
 
@@ -23,14 +24,30 @@ class Clipboard(models.Model):
         db_table = 'clipboard'
 
 
+def create_file_path(instance: 'File', filename: str):
+    """
+    创建文件保存路径
+
+    :param instance: File 对象
+    :param filename: 文件名
+    """
+    file_dir = settings.FILE_APP.get('FILE_SAVE_DIR') / datetime.now().strftime('%Y-%m-%d')
+    if not file_dir.exists():
+        file_dir.mkdir(parents=True)
+    file_path = file_dir / f'{filename}.{uuid.uuid4().hex}'
+    return file_path
+
+
 class ClipboardContent(models.Model):
     """剪切板内容模型"""
     content_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     clipboard = models.ForeignKey(Clipboard, on_delete=models.CASCADE, related_name='contents')
-    content_type = models.IntegerField(choices=ClipboardContentType.to_choices(), default=ClipboardContentType.text.value)
+    content_type = models.IntegerField(choices=ClipboardContentType.to_choices(),
+                                       default=ClipboardContentType.text.value)
     text_content = models.TextField(blank=True, null=True)
-    file_metadata = JSONField(blank=True, null=True)
-    file_path = models.CharField(max_length=255, blank=True, null=True)
+    file_name = models.CharField(max_length=255, blank=True, null=True)
+    file_size = models.IntegerField(blank=True, null=True)
+    file_path = models.FileField(upload_to=create_file_path, blank=True, null=True)
     created_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
 
