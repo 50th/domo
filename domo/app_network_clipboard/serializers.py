4cc0a8d372558ac6ba4e08ed8549193dab0742ba
jo <754514192@qq.com>
2025-04-10 18:37:28 +0800

    添加时 name 可以为空，使用 id 值；
    更新时，限制更新字段，非密码共享时，密码置为空；

--- domo/app_network_clipboard/serializers.py
+++ domo/app_network_clipboard/serializers.py
@@ -5,15 +5,34 @@ from constants.constants import ClipboardPrivacy
 
 
 class ClipboardSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=100, required=False)
+    created_username = serializers.ReadOnlyField(source='created_user.username', default='')
     created_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', required=False, read_only=True)
 
     def validate(self, attrs):
         privacy_type = attrs['privacy_type']
         password = attrs['password']
-        if privacy_type == ClipboardPrivacy.shared_pass.value and (password is None or not password.strip()):
-            raise serializers.ValidationError('当选择使用密码共享时，密码不能为空')
+        if privacy_type == ClipboardPrivacy.shared_pass.value:
+            if password is None or not password.strip():
+                raise serializers.ValidationError('当选择使用密码共享时，密码不能为空')
+            if len(password) < 6:
+                raise serializers.ValidationError('当选择使用密码共享时，密码长度不能小于6位')
         return attrs
+
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+        if instance.name is None or not instance.name.strip():
+            instance.name = instance.clipboard_id
+            instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        if validated_data['privacy_type'] != ClipboardPrivacy.shared_pass.value:
+            validated_data['password'] = None
+        return super().update(instance, validated_data)
 
     class Meta:
         model = Clipboard
-        fields = '__all__'
+        fields = ['clipboard_id', 'name', 'privacy_type', 'password', 'content', 'created_user', 'created_username',
+                  'created_time']
+        read_only_fields = ['clipboard_id', 'created_user', 'created_username', 'created_time']
--- domo/app_network_clipboard/views.py
+++ domo/app_network_clipboard/views.py
@@ -5,7 +5,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from app_network_clipboard.models import Clipboard
 from app_network_clipboard.serializers import ClipboardSerializer
-from constants.reponse_codes import ResponseCode
 
 
 class ClipboardViewSet(ModelViewSet):
@@ -20,29 +19,9 @@ class ClipboardViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        # 添加创建用户信息
         if request.user and request.user.is_authenticated:
             serializer.validated_data['created_user'] = request.user
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.author == request.user or request.user.is_superuser:
-            partial = kwargs.pop('partial', False)
-            serializer = self.get_serializer(instance, data=request.data, partial=partial)
-            serializer.is_valid(raise_exception=True)
-            if not instance.author and request.user.is_superuser:
-                serializer.validated_data['author'] = request.user
-            self.perform_update(serializer)
-            # 记录更新操作
-            edit_log = ArticleEditLog.objects.create(article=instance, edit_user=request.user)
-            edit_log.save()
-            if getattr(instance, '_prefetched_objects_cache', None):
-                # If 'prefetch_related' has been applied to a queryset, we need to
-                # forcibly invalidate the prefetch cache on the instance.
-                instance._prefetched_objects_cache = {}
-
-            return Response(serializer.data)
-        else:
-            return Response(ResponseCode.PERMISSION_DENIED)
